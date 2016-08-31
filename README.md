f5fb01cec740ff1bfcc5796276f4af8e24a74061
Cristofer <cristofer_flort@hotmail.com>
2016-08-31 00:50:24 -0500

    Poyecto finalizado
    Proyecto completo, funcionalidad total

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # PostApp
 Aplicacion en AngularJs
+
+Esta aplicacion es de prueba para ver como funciona Angularjs consumiendo API'S REST
+
+Utilizamos una API fake: <a href="https://jsonplaceholder.typicode.com/">Jsonplaceholder</a>
+Esta API nos permite simular los metodos para hacer peticiones a un servidor tales como:
+<ul>
+	<li>POST 	-> Para agregar un nuevo recurso</li>
+	<li>GET  	-> Para consultar un recurso en especifico</li>
+	<li>PUT  	-> Para actualizar un recurso</li>
+	<li>DELETE 	-> Para eliminar un recurso espeficio</li>
+</ul>
+
+Se utilizaron los siguientes componentes de angular:
+<ul>
+	<li>AngularRoute</li>
+	<li>AngularResource</li>
+	<li>AngularLocation</li>
+</ul>
+
+Esta aplicacion de prueba te permite crea, consultar, actualizar y eliminar algun post que estan en la API de json placeholder.
+
+By:  José Christofer Flores Ortega
--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,13 @@
 angular.module("FinalApp")
-.controller('MainController', ['$scope','$resource', function($scope,$resource){
-	Post = $resource('https://jsonplaceholder.typicode.com/posts/:id',{id: '@id'});
+.controller('MainController', ['$scope','$resource','PostResource', function($scope,$resource,PostResource){
 	User = $resource('https://jsonplaceholder.typicode.com/users/:id',{id: '@id'});
 
 	$scope.users = User.query();
-	$scope.posts = Post.query();
+	$scope.posts = PostResource.query();
 	// query -> GET /post -> Un (importante)arreglo de Post
 
 	$scope.removePost = function(post){
-		Post.delete({id: post.id},function(data){
+		PostResource.delete({id: post.id},function(data){
 			console.log(data);
 		});
 		$scope.posts = $scope.posts.filter(function(element){
@@ -16,7 +15,24 @@ angular.module("FinalApp")
 		});
 	}
 }])
-.controller('PostController', ['$scope','$resource','$routeParams', function($scope,$resource,$routeParams){
-	Post = $resource('https://jsonplaceholder.typicode.com/posts/:id',{id: '@id'});
-	$scope.post = Post.get({id: $routeParams.id})
+.controller('PostController', ['$scope','$resource','$routeParams','PostResource','$location', function($scope,$resource,$routeParams,PostResource,$location){
+	$scope.title = "Edit this post";
+	$scope.post = PostResource.get({id: $routeParams.id})
+	$scope.newPost = function(){
+		PostResource.update({id: $scope.post.id},{data: $scope.post},function(data){
+			console.log(data);
+			$location.path('/');
+		});
+	}
+}])
+.controller('NewPostController', ['$scope','$resource','PostResource','$location', function($scope,$resource,PostResource,$location){	
+	$scope.post = {};
+	$scope.title = "Create your own post";
+	$scope.newPost = function(){
+		PostResource.save({data: $scope.post},function(data){
+			console.log(data);
+			$location.path('/');
+		});
+	}
+	
 }])
